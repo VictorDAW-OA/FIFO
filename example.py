0924156103f250bd8aa9196db7b17d4dd3670ff5
fasturnino <garciavictor1111@gmail.com>
2024-11-27 19:05:54 +0100

    add new logic

--- example.py
+++ example.py
@@ -4,13 +4,17 @@ FATAL_RESULT = 'no'
 
 def check_if_aprove_or_not() -> str:
 
-    result = input('¿Eres del Madrid?')
+    result = input('¿Eres del Madrid? ')
 
 
     if isinstance(result, str):
         if result.lower() == 'yes':
             return 'Aprobado'
-
+        for i in range(0, 15):
+            r = input('¿Estás seguro? ')
+            if isinstance(r, str) and r == 'yes':
+                return 'SUSPENDIDO'
         return 'SUSPENDIDO'    
     
 
+check_if_aprove_or_not()
